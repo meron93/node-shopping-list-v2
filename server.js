7b64aab4159a6be5431bb6101e9935c313908a88
meron93 <meronyemane18@gmail.com>
2017-04-06 16:49:45 -0700

    implemented recipes POST

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ Recipes.create(
 Recipes.create(
   'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
 
+console.log(Recipes);
+
 // when the root of this router is called with GET, return
 // all current ShoppingList items
 app.get('/shopping-list', (req, res) => {
@@ -47,8 +49,25 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
+app.post('/recipes', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing ${field} in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
+
+
 
 app.get('/recipes', (req, res) => {
+  console.info("asdfadsf");
   res.json(Recipes.get());
 })
 
